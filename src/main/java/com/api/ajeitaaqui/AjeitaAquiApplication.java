070a7ffb3b25533a078e4151cd2314645a18992b
CayoViegas <cayo.viegas@ccc.ufcg.edu.br>
2023-08-24 09:14:19 -0300

    projeto criado

---
+++ src/main/java/com/api/ajeitaaqui/AjeitaAquiApplication.java
@@ -0,0 +1,13 @@
+package com.api.ajeitaaqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjeitaAquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjeitaAquiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/ajeitaaqui/AjeitaAquiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ajeitaaqui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjeitaAquiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
